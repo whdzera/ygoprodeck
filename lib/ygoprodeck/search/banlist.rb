a33452a1955ad847691c2c7a296ee45b93eac6fa
whdzera <whdzera@gmail.com>
2025-09-21 02:33:07 +0700

    feat: banlist md

--- lib/ygoprodeck/search/banlist.rb
+++ lib/ygoprodeck/search/banlist.rb
@@ -1,5 +1,11 @@
+require 'net/http'
+require 'json'
+require 'uri'
+
 module Ygoprodeck
   class Banlist
+    API_BASE = 'https://ygoprodeck.com/api/banlist'
+
     def self.fetch_banlist(format)
       url = "#{Ygoprodeck::Endpoint.is}cardinfo.php?banlist=#{format}"
       uri = URI(url)
@@ -13,16 +19,15 @@ module Ygoprodeck
         else
           load
         end
-        
-      rescue JSON::ParserError => e
+      rescue JSON::ParserError
         { 'error' => 'Failed to parse JSON response' }
-      rescue SocketError => e
+      rescue SocketError
         { 'error' => 'Network connection error' }
       rescue StandardError => e
         { 'error' => "An unexpected error occurred: #{e.message}" }
       end
     end
-    
+
     def self.tcg
       fetch_banlist('tcg')
     end
@@ -33,6 +38,41 @@ module Ygoprodeck
     
     def self.goat
       fetch_banlist('goat')
+    end
+
+    def self.md
+      
+      dates_uri = URI("#{API_BASE}/getBanListDates.php")
+      dates_res = Net::HTTP.get(dates_uri)
+      dates = JSON.parse(dates_res)
+
+      master_duel_dates = dates.select { |d| d['type'] == 'Master Duel' }
+      return { 'error' => 'No Master Duel dates found' } if master_duel_dates.empty?
+
+      latest_date = master_duel_dates.map { |d| d['date'] }.max
+
+      list_uri = URI("#{API_BASE}/getBanList.php?list=Master%20Duel&date=#{latest_date}")
+      req = Net::HTTP::Get.new(list_uri)
+      req['User-Agent'] = 'Mozilla/5.0'
+
+      res = Net::HTTP.start(list_uri.host, list_uri.port, use_ssl: true) do |http|
+        http.request(req)
+      end
+
+      load = JSON.parse(res.body)
+
+      if load.is_a?(Hash) && load.key?('error')
+        { 'error' => load['error'] }
+      else
+        load
+      end
+    rescue JSON::ParserError
+      { 'error' => 'Failed to parse JSON response' }
+    rescue SocketError
+      { 'error' => 'Network connection error' }
+    rescue StandardError => e
+      { 'error' => "An unexpected error occurred: #{e.message}" }
+      
     end
   end
 end
